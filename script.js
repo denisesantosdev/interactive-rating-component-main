9328bbe546b53e66ef77c22f0ecebb6d443de635
denisesantosdev <denisesantosdev@gmail.com>
2023-03-30 18:01:25 -0300

    better javascript logic

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const startStateCard = document.querySelector('.rating-card__start-state')
-const endStateCard = document.querySelector('.rating-card__end-state')
-const userRating = document.querySelector('#user-rating')
-const checkedInput = document.querySelector('.rating-card__numbers input:checked')
+const startStateCard = document.querySelector(".rating-card__start-state");
+const endStateCard = document.querySelector(".rating-card__end-state");
+const userRating = document.querySelector("#user-rating");
 
-let userRatingValue = checkedInput.value
+let userRatingValue;
 
-document.querySelector('.rating-card__numbers').addEventListener('change', (event) => {
+document
+  .querySelector(".rating-card__numbers")
+  .addEventListener("change", (event) => {
     userRatingValue = event.target.value;
-})
+  });
 
-document.querySelector('.rating-card__btn').addEventListener('click', () => {
-    startStateCard.classList.add('hidden')
-    endStateCard.classList.remove('hidden')
-    userRating.innerText = userRatingValue
-})
+document.querySelector(".rating-card__btn").addEventListener("click", (event) => {
+  if (userRatingValue === undefined) return;
+  startStateCard.classList.add("hidden");
+  endStateCard.classList.remove("hidden");
+  userRating.innerText = userRatingValue;
+});
